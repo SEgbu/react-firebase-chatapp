84885471ebe131c4ce4b166250723b53af2a22ec
Sincere <sincerityegbu@gmail.com>
2023-11-05 10:55:22 +0000

    Added Signing Out

--- src/ChatRoom.tsx
+++ src/ChatRoom.tsx
@@ -1,7 +1,11 @@
-export const ChatRoom = () => {
+import React from "react"
+import { SignOut } from "./SignOut"
+
+export const ChatRoom : React.FC = () => {
     return (
         <div>
             <p>Chat Room</p>
+            <SignOut/>
         </div>
     )
 }
--- src/SignOut.tsx
+++ src/SignOut.tsx
@@ -1,3 +1,10 @@
-const SignOut = () => {
-    
+import React from "react"
+import { auth } from "./firebase"
+
+export const SignOut : React.FC = () => {
+    return (
+        <>
+            <button onClick={() => auth.signOut()}>Sign Out</button>
+        </>
+    )
 }
